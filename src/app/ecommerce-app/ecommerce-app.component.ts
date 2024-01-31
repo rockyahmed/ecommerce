5d944dc6dce8016c9be2fb79f5cf4532a465d653
rockyahmed <rockybd1995@gmail.com>
2024-01-31 22:38:26 +0600

    fillter working

--- src/app/ecommerce-app/ecommerce-app.component.ts
+++ src/app/ecommerce-app/ecommerce-app.component.ts
@@ -13,25 +13,81 @@ import { menuCategory } from '../models/menu-category-model';
 })
 export class EcommerceAppComponent implements OnInit {
 
-  // formDataArray: Array<menuCategory> = [];
 
-  // constructor() {}
+  // menuCreateForm = this.formBuilder.group({
+  //   categoryName: ['', Validators.required],
+  //   categoryID: ['', Validators.required],
+  //   fkParentID: ['',]
+  // });
+
+  // formDataArray: Array<menuCategory> = [];
+  // subsections: Array<menuCategory> = []
+
+  // constructor(private formBuilder: FormBuilder) {}
 
   // ngOnInit() {
-
-  //   console.log(this.formDataArray)
-
   //   const storedData = localStorage.getItem('form-data');
 
   //   if (storedData !== null) {
   //     this.formDataArray = JSON.parse(storedData);
+  //     // this.subsections = this.buildSubsections(this.formDataArray);
   //   }
-    
   // }
 
-  // hasMatchingChild(formData: menuCategory): boolean {
-  //   return this.formDataArray.some(formDataChild => formDataChild.fkParentID == formData.categoryID);
+  // onSubmit() {
+  //   console.log(this.formDataArray);
+  //   console.log(this.subsections);
+  
+  //   const formData = this.menuCreateForm.value as menuCategory;
+  
+  //   const categoryIdExists = this.formDataArray.some(
+  //     (entry: menuCategory) => entry.categoryID === formData.categoryID
+  //   );
+  
+  //   if (categoryIdExists) {
+  //     window.alert(
+  //       `Category ID ${formData.categoryID} already exists. Entry again not possible.`
+  //     );
+  //   } else {
+  //     this.formDataArray.push(formData);
+  
+  //     // Find items in formDataArray where categoryID matches fkParentID
+  //     const matchingSubsections = this.formDataArray.filter(
+  //       (entry: menuCategory) => entry.categoryID === formData.fkParentID
+  //     );
+
+  
+  //     // Add the matching subsections to the subsections array
+  //     this.subsections.push(...matchingSubsections);
+  
+  //     localStorage.setItem('form-data', JSON.stringify(this.formDataArray));
+  
+  //     this.menuCreateForm.reset();
+  //   }
   // }
+  
+  // buildSubsections(formDataArray: menuCategory[]): menuCategory[] {
+  //   const result: menuCategory[] = [];
+  
+  //   formDataArray.forEach(formData => {
+  //     const matchingChild = formDataArray.find(child => child.fkParentID === formData.categoryID);
+  
+  //     if (matchingChild) {
+  //       const existingSubsection = result.find(item => item.categoryID === formData.categoryID);
+  
+  //       if (existingSubsection) {
+  //         existingSubsection.subsections = existingSubsection.subsections || [];
+  //         existingSubsection.subsections.push(matchingChild);
+  //       } else {
+  //         const newSubsection: menuCategory = { ...formData, subsections: [matchingChild] };
+  //         result.push(newSubsection);
+  //       }
+  //     }
+  //   });
+  
+  //   return result;
+  // }
+
 
   formDataArray: Array<menuCategory> = [];
   emptyfkid: Array<menuCategory> = [];
