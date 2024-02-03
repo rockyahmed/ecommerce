0c1eb7473e474bbf6344be29e448e6b06b92932b
rockyahmed <rockybd1995@gmail.com>
2024-02-03 18:11:04 +0600

    recursive way subsections create done

--- src/app/ecommerce-app/ecommerce-app.component.ts
+++ src/app/ecommerce-app/ecommerce-app.component.ts
@@ -12,84 +12,12 @@ import { menuCategory } from '../models/menu-category-model';
   styleUrl: './ecommerce-app.component.scss',
 })
 export class EcommerceAppComponent implements OnInit {
-  // menuCreateForm = this.formBuilder.group({
-  //   categoryName: ['', Validators.required],
-  //   categoryID: ['', Validators.required],
-  //   fkParentID: ['',]
-  // });
-
-  // formDataArray: Array<menuCategory> = [];
-  // subsections: Array<menuCategory> = []
-
-  // constructor(private formBuilder: FormBuilder) {}
-
-  // ngOnInit() {
-  //   const storedData = localStorage.getItem('form-data');
-
-  //   if (storedData !== null) {
-  //     this.formDataArray = JSON.parse(storedData);
-  //     // this.subsections = this.buildSubsections(this.formDataArray);
-  //   }
-  // }
-
-  // onSubmit() {
-  //   console.log(this.formDataArray);
-  //   console.log(this.subsections);
-
-  //   const formData = this.menuCreateForm.value as menuCategory;
-
-  //   const categoryIdExists = this.formDataArray.some(
-  //     (entry: menuCategory) => entry.categoryID === formData.categoryID
-  //   );
-
-  //   if (categoryIdExists) {
-  //     window.alert(
-  //       `Category ID ${formData.categoryID} already exists. Entry again not possible.`
-  //     );
-  //   } else {
-  //     this.formDataArray.push(formData);
-
-  //     // Find items in formDataArray where categoryID matches fkParentID
-  //     const matchingSubsections = this.formDataArray.filter(
-  //       (entry: menuCategory) => entry.categoryID === formData.fkParentID
-  //     );
-
-  //     // Add the matching subsections to the subsections array
-  //     this.subsections.push(...matchingSubsections);
-
-  //     localStorage.setItem('form-data', JSON.stringify(this.formDataArray));
-
-  //     this.menuCreateForm.reset();
-  //   }
-  // }
-
-  // buildSubsections(formDataArray: menuCategory[]): menuCategory[] {
-  //   const result: menuCategory[] = [];
-
-  //   formDataArray.forEach(formData => {
-  //     const matchingChild = formDataArray.find(child => child.fkParentID === formData.categoryID);
-
-  //     if (matchingChild) {
-  //       const existingSubsection = result.find(item => item.categoryID === formData.categoryID);
-
-  //       if (existingSubsection) {
-  //         existingSubsection.subsections = existingSubsection.subsections || [];
-  //         existingSubsection.subsections.push(matchingChild);
-  //       } else {
-  //         const newSubsection: menuCategory = { ...formData, subsections: [matchingChild] };
-  //         result.push(newSubsection);
-  //       }
-  //     }
-  //   });
-
-  //   return result;
-  // }
-
+ 
   formDataArray: Array<menuCategory> = [];
 
-  modifiedData: menuCategory[] = [];
+  // modifiedData: menuCategory[] = [];
 
-  // modifiedData: Array<menuCategory> = [];
+  modifiedData: Array<menuCategory> = [];
 
   constructor() {}
 
@@ -98,45 +26,75 @@ export class EcommerceAppComponent implements OnInit {
 
     const storedData = localStorage.getItem('form-data');
 
+    // if (storedData !== null) {
+    //   this.formDataArray = JSON.parse(storedData);
+
+    //   const emptyfkid = this.formDataArray.filter(
+    //     (item) => item.fkParentID === null
+    //   );
+
+    //   for (const item of emptyfkid) {
+    //     const category = {} as menuCategory;
+    //     category.categoryID = item.categoryID;
+    //     category.categoryName = item.categoryName;
+    //     const childrenItem = this.formDataArray.filter(
+    //       (x) => x.fkParentID == item.categoryID
+    //     );
+    //     for (const c of childrenItem) {
+    //       const children2 = this.formDataArray.filter(
+    //         (y) => y.fkParentID == c.categoryID
+    //       );
+    //       const category2 = {} as menuCategory;
+    //       category2.categoryID = c.categoryID;
+    //       category2.categoryName = c.categoryName;
+    //       c.subsections = [...children2];
+    //     }
+
+    //     category.subsections = [...childrenItem];
+    //     this.modifiedData.push(category);
+
+    //     console.log('Modified Data Array: ', this.modifiedData);
+    //   }
+    // }
+
+
     if (storedData !== null) {
       this.formDataArray = JSON.parse(storedData);
-
-      const emptyfkid = this.formDataArray.filter(
-        (item) => item.fkParentID === null
-      );
-
-      for (const item of emptyfkid) {
-        const category = {} as menuCategory;
-        category.categoryID = item.categoryID;
-        category.categoryName = item.categoryName;
-        const childrenItem = this.formDataArray.filter(
-          (x) => x.fkParentID == item.categoryID
-        );
-        for (const c of childrenItem) {
-          const children2 = this.formDataArray.filter(
-            (y) => y.fkParentID == c.categoryID
-          );
-          const category2 = {} as menuCategory;
-          category2.categoryID = c.categoryID;
-          category2.categoryName = c.categoryName;
-          c.subsections = [...children2];
+    
+      const buildHierarchy = (parentId: number | null | undefined): menuCategory[] => {
+        const result: menuCategory[] = [];
+    
+        for (const item of this.formDataArray) {
+          // Handle null, undefined, or empty string for fkParentID
+          if (item.fkParentID == parentId || (parentId == undefined && !item.fkParentID)) {
+            const category: menuCategory = {
+              categoryID: item.categoryID,
+              categoryName: item.categoryName,
+              fkParentID: item.fkParentID,
+            };
+    
+            const children = buildHierarchy(item.categoryID);
+            if (children.length > 0) {
+              category.subsections = children;
+            }
+    
+            result.push(category);
+          }
         }
-
-        category.subsections = [...childrenItem];
-        this.modifiedData.push(category);
-
-        console.log('Modified Data Array: ', this.modifiedData);
-      }
+    
+        return result;
+      };
+    
+      // Build the hierarchy starting from the root (categories with fkParentID === null or undefined)
+      const topLevelCategories = buildHierarchy(null);
+    
+      // Assign the result to this.modifiedData
+      this.modifiedData = topLevelCategories;
+    
+      console.log('Modified Data Array: ', this.modifiedData);
     }
+
+
   }
 
-  hasMatchingChild(formData: menuCategory): boolean {
-    return this.formDataArray.some(
-      (formDataChild) => formDataChild.fkParentID == formData.categoryID
-    );
-  }
-
-  // getFilteredData(parentId: number | null): menuCategory[] {
-  //   return this.formDataArray.filter(x => x.fkParentID === parentId);
-  // }
 }
