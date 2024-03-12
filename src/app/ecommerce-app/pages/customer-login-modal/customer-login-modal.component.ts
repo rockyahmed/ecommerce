5f3baaaa12354a1a10a52b3d9c35f65e0066e249
rockyahmed <rockybd1995@gmail.com>
2024-03-12 23:02:33 +0600

    login form working

--- src/app/ecommerce-app/pages/customer-login-modal/customer-login-modal.component.ts
+++ src/app/ecommerce-app/pages/customer-login-modal/customer-login-modal.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-customer-login-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customer-login-modal.component.html',
-  styleUrl: './customer-login-modal.component.scss'
+  styleUrl: './customer-login-modal.component.scss',
 })
-export class CustomerLoginModalComponent {
+export class CustomerLoginModalComponent implements OnInit {
   title = '';
-  constructor(
-    public bsModalRef: BsModalRef
-  ) {
+  loginRegister: boolean = false;
 
+  loginForm!: FormGroup;
+  
+
+  constructor(public bsModalRef: BsModalRef, public fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      ],
+      password: [
+        Validators.required,
+        Validators.pattern(
+          '(?=\\D*\\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}'
+        ),
+      ],
+    });
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.value)
+  }
+
+  registerForm() {
+    this.loginRegister = !this.loginRegister;
   }
 }
